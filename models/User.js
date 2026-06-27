c259b3e2fa5923856030ec335a414076baf4b4ab
Basanta Khand <basantakhand55@gmail.com>
2026-06-27 10:00:00 +0000

    Add isActive field to user model

--- models/User.js
+++ models/User.js
@@ -41,6 +41,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
   {
     timestamps: true, // adds createdAt and updatedAt
