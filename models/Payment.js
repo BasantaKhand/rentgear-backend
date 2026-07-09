abdc5fd20569184ac422b085519f94f6fdcb8e7e
Basanta Khand <basantakhand55@gmail.com>
2026-07-09 10:30:00 +0000

    Encrypt payment transaction id at rest

--- models/Payment.js
+++ models/Payment.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { encrypt, decrypt } = require('../utils/encryption');
 
 const paymentSchema = new mongoose.Schema({
   booking: {
@@ -24,11 +25,17 @@ const paymentSchema = new mongoose.Schema({
   transactionId: {
     type: String,
     default: null,
+    set: encrypt, // encrypted at rest
+    get: decrypt,
   },
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
+
+// Ensure the transaction id is decrypted when serialized.
+paymentSchema.set('toJSON', { getters: true });
+paymentSchema.set('toObject', { getters: true });
 
 module.exports = mongoose.model('Payment', paymentSchema);
